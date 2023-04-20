6ec36336dbdcd64e31c5fd75cdfaf632bafb0a01
aditya-nambiar2007 <99115432+aditya-nambiar2007@users.noreply.github.com>
2023-04-20 13:01:41 +0530

    Add files via upload

---
+++ game.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+func print(s string) {
+	fmt.Println("\n" + s)
+	fmt.Println()
+}
+
+func replace(str string) string {
+	out := []rune(str)
+	for i := 1; i < len(out); i += 2 {
+		out[i] = '_'
+	}
+	return string(out)
+}
+
+var ca, q = 0, 0
+var array = [...]string{"Aardvark", "Albatross", "Alligator", "Alpaca", "Ant", "Anteater", "Antelope", "Ape", "Armadillo", "Donkey", "Baboon", "Badger", "Barracuda", "Bat", "Bear", "Beaver", "Bee", "Bison", "Boar", "Buffalo", "Butterfly", "Camel", "Capybara", "Caribou", "Cassowary", "Cat", "Caterpillar", "Cattle", "Chamois", "Cheetah", "Chicken", "Chimpanzee", "Chinchilla", "Chough", "Clam", "Cobra", "Cockroach", "Cod", "Cormorant", "Coyote", "Crab", "Crane", "Crocodile", "Crow", "Curlew", "Deer", "Dinosaur", "Dog", "Dogfish", "Dolphin", "Dotterel", "Dove", "Dragonfly", "Duck", "Dugong", "Dunlin", "Eagle", "Echidna", "Eel", "Eland", "Elephant", "Elk", "Emu", "Falcon", "Ferret", "Finch", "Fish", "Flamingo", "Fly", "Fox", "Frog", "Gaur", "Gazelle", "Gerbil", "Giraffe", "Gnat", "Gnu", "Goat", "Goldfinch", "Goldfish", "Goose", "Gorilla", "Goshawk", "Grasshopper", "Grouse", "Guanaco", "Gull", "Hamster", "Hare", "Hawk", "Hedgehog", "Heron", "Herring", "Hippopotamus", "Hornet", "Horse", "Human", "Hummingbird", "Hyena", "Ibex", "Ibis", "Jackal", "Jaguar", "Jay", "Jellyfish", "Kangaroo", "Kingfisher", "Koala", "Kookabura", "Kouprey", "Kudu", "Lapwing", "Lark", "Lemur", "Leopard", "Lion", "Llama", "Lobster", "Locust", "Loris", "Louse", "Lyrebird", "Magpie", "Mallard", "Manatee", "Mandrill", "Mantis", "Marten", "Meerkat", "Mink", "Mole", "Mongoose", "Monkey", "Moose", "Mosquito", "Mouse", "Mule", "Narwhal", "Newt", "Nightingale", "Octopus", "Okapi", "Opossum", "Oryx", "Ostrich", "Otter", "Owl", "Oyster", "Panther", "Parrot", "Partridge", "Peafowl", "Pelican", "Penguin", "Pheasant", "Pig", "Pigeon", "Pony", "Porcupine", "Porpoise", "Quail", "Quelea", "Quetzal", "Rabbit", "Raccoon", "Rail", "Ram", "Rat", "Raven", "Red deer", "Red panda", "Reindeer", "Rhinoceros", "Rook", "Salamander", "Salmon", "Sand Dollar", "Sandpiper", "Sardine", "Scorpion", "Seahorse", "Seal", "Shark", "Sheep", "Shrew", "Skunk", "Snail", "Snake", "Sparrow", "Spider", "Spoonbill", "Squid", "Squirrel", "Starling", "Stingray", "Stinkbug", "Stork", "Swallow", "Swan", "Tapir", "Tarsier", "Termite", "Tiger", "Toad", "Trout", "Turkey", "Turtle", "Viper", "Vulture", "Wallaby", "Walrus", "Wasp", "Weasel", "Whale", "Wildcat", "Wolf", "Wolverine", "Wombat", "Woodcock", "Woodpecker", "Worm", "Wren", "Yak", "Zebra"}
+
+func main() {
+	fmt.Println(
+		"Welcome To Animal Guesser:\n",
+		"-> Guess The Animal\n",
+		"-> Correct Answer : 1 point \n -> Wrong Answer : 0 points\n",
+		"-> Type \"exit\" to exit")
+	fmt.Println("")
+	for {
+		time.Sleep(500 * time.Millisecond)
+		q++
+		var animal string = array[rand.Int()%len(array)]
+		fmt.Println("Ques: " + replace(animal))
+		var str string
+		fmt.Print("Ans : ")
+		fmt.Scan(&str)
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println("Ans :" + animal)
+		time.Sleep(500 * time.Millisecond)
+		if strings.ToUpper(str) == strings.ToUpper(animal) {
+			print("You Are Correct")
+			ca++
+		} else if str == "exit" {
+			q--
+			fmt.Println("")
+			fmt.Print("Questions : ", q, "\n")
+			fmt.Print("Correct Answers : ", ca, "\n\n", "Thanks For Trying The Game !")
+			time.Sleep(2 * time.Second)
+			break
+		} else {
+			print("You Are Incorrect")
+		}
+	}
+
+}
